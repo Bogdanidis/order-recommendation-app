89e580ea136d63386cc71e8e707b939484fc4a6d
Bogdanidis Nikos <boganitis99@gmail.com>
2024-08-23 15:56:28 +0300

    Initiliazied project, added db schema creation script.

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,67 @@
+DROP SCHEMA IF EXISTS oder_app;
+
+CREATE SCHEMA oder_app;
+
+USE oder_app;
+
+CREATE TABLE customers (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    -- username VARCHAR(255) NOT NULL,
+    -- password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE orders (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    CONSTRAINT fk_customer
+        FOREIGN KEY(user_id)
+        REFERENCES customers(id)
+);
+
+CREATE TABLE products (
+      id BIGINT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(255) NOT NULL,
+      description VARCHAR(255) NOT NULL,
+      price VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE order_items (
+       id BIGINT PRIMARY KEY AUTO_INCREMENT,
+       order_id BIGINT NOT NULL,
+       CONSTRAINT fk_order
+            FOREIGN KEY(order_id)
+            REFERENCES orders(id),
+       product_id BIGINT NOT NULL,
+       CONSTRAINT fk_product
+           FOREIGN KEY(product_id)
+           REFERENCES products(id),
+        quantity INT NOT NULL
+
+);
+
+
+
+CREATE TABLE categories (
+      id BIGINT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(255) NOT NULL,
+      description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product_category (
+    category_id BIGINT NOT NULL,
+       CONSTRAINT fk_category
+            FOREIGN KEY(category_id)
+            REFERENCES categories(id),
+       product_id BIGINT NOT NULL,
+       CONSTRAINT fk_category_product
+           FOREIGN KEY(product_id)
+           REFERENCES products(id),
+    PRIMARY KEY(category_id,product_id)
+
+);
